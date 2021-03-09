9e6ffb61be5a717e7838e12227f5e4baad539ddb
Rajesh Uppala <rajeshuppala1449@gmail.com>
2021-03-09 20:30:22 +0530

    spring MVC

---
+++ SpringMVC-Afternoon/ordermgmt/src/main/java/services/OrderService.java
@@ -0,0 +1,69 @@
+package com.rajesh.ordermgmt.services;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.rajesh.ordermgmt.model.Order;
+import com.rajesh.ordermgmt.model.OrderItem;
+import com.rajesh.ordermgmt.model.OrdersManagement;
+
+@Service
+public class OrderService {
+
+    private int lastOrderIdGiven, lastItemIdGiven;
+    private OrdersManagement orders;
+
+    public OrderService(){
+        this.lastOrderIdGiven = 0;
+        this.lastItemIdGiven = 0;
+        orders = new OrdersManagement();
+    }
+
+    public Order createOrder(Order order){
+        order.setId(++lastItemIdGiven);
+       // orders.setOrdersList(order);
+        return order;
+    }
+
+    public List<Order> getAllOrders(){
+        return orders.getOrdersList();
+    }
+
+    public Order addItemToOrder(int orderId, OrderItem item){
+        Order currentOrder = orders.getOrder(orderId);
+        item.setId(++lastOrderIdGiven);
+        if(currentOrder.addItem(item)) {
+            return currentOrder;
+        }
+        return null;
+    }
+
+    public List<OrderItem> getOrderItems(int orderId){
+        Order currentOrder = orders.getOrder(orderId);
+        return currentOrder.getItemsList();
+    }
+
+    public boolean checkOrderItem(int orderId){
+        Order currentOrder = orders.getOrder(orderId);
+        if(currentOrder == null){
+            return false;
+        }
+        return true;
+    }
+
+    public boolean removeItemFromOrder(int orderId, int itemId){
+        Order currentOrder = orders.getOrder(orderId);
+        return currentOrder.removeItem(itemId);
+    }
+
+    public OrderItem updateItem(int itemId, int orderId, OrderItem item){
+        Order currentOrder = orders.getOrder(orderId);
+        item.setId(++lastOrderIdGiven);
+        return currentOrder.updateOrderItem(itemId, item);
+    }
+
+    public boolean removeOrder(int orderId){
+        return orders.removeOrder(orderId);
+    }
+}
