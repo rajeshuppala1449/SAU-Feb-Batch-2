da7fb507a3bd7720b844bf8859ae0c165e624e94
Rajesh Uppala <rajeshuppala1449@gmail.com>
2021-03-11 12:58:10 +0530

    Junit

---
+++ Junit, Mockito, Logging - Afternoon/src/main/java/mavenBatch/junit/Log4jTest.java
@@ -0,0 +1,14 @@
+package mavenBatch.junit;
+
+public class Log4jTest {
+	
+	private String message;
+	   
+	public void setMessage(String message){
+		this.message  = message;
+	}
+	public void getMessage() {
+		System.out.println("Your Message : " + message);
+	}
+
+}
